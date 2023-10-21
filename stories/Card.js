c84203a43ec4d7acc6d3f8680a951e8de651c081
john-santhosh <johnsantosh2@gmail.com>
2023-10-21 18:18:30 +0530

    first story

---
+++ stories/Card.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import { options } from "./constants";
+
+export const Card = ({
+  children = "I'm card",
+  color = "primary",
+  size = "sm",
+}) => {
+  return <div>{children}</div>;
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(options.colors),
+  size: PropTypes.oneOf(options.sizes),
+};
+
+export default Card;
