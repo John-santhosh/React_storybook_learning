21ede573a5f553f577c19388d26bdaf5db6f3f92
john-santhosh <johnsantosh2@gmail.com>
2023-10-30 11:00:29 +0530

    added default story styles and added auto docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,28 @@ import "minireset.css";
 import "../styles/global.css";
 import "../styles/token.css";
 
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px",
+  background: "red",
+};
+
+// this is useful when we need to represent our stories with some default styles
+export const decorators = [
+  (Story) => {
+    return (
+      <div style={styles}>
+        <Story />
+      </div>
+    );
+  },
+];
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -6,6 +6,8 @@ export default {
   args: {
     children: "Iam a card by default in the stories(default)",
   },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
   argTypes: {
     color: {
       description: "**options:**",
